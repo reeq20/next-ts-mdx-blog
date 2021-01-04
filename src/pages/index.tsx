56e92923499955cd2d4b87aba8a0a887d84dd8a6
reeq20 <rikusugawara@fabrec.jp>
2021-01-04 21:24:40 +0900

    fix svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,19 +26,19 @@ const Home: NextPage<Props> = () => {
                 <div className="p-4 lg:w-1/2 md:w-full">
                   <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
                     <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 flex-shrink-0">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-8 h-8"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                      >
-                        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                      </svg>
+                      {/*<svg*/}
+                      {/*  fill="none"*/}
+                      {/*  stroke="currentColor"*/}
+                      {/*  strokeLinecap="round"*/}
+                      {/*  strokeLinejoin="round"*/}
+                      {/*  strokeWidth="2"*/}
+                      {/*  className="w-8 h-8"*/}
+                      {/*  viewBox="0 0 24 24"*/}
+                      {/*  width="24"*/}
+                      {/*  height="24"*/}
+                      {/*>*/}
+                      {/*  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />*/}
+                      {/*</svg>*/}
                     </div>
                     <div className="flex-grow">
                       <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
@@ -50,19 +50,19 @@ const Home: NextPage<Props> = () => {
                       </p>
                       <a className="mt-3 text-blue-500 inline-flex items-center">
                         Learn More
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          className="w-4 h-4 ml-2"
-                          viewBox="0 0 24 24"
-                          width="16"
-                          height="16"
-                        >
-                          <path d="M5 12h14M12 5l7 7-7 7" />
-                        </svg>
+                        {/*<svg*/}
+                        {/*  fill="none"*/}
+                        {/*  stroke="currentColor"*/}
+                        {/*  strokeLinecap="round"*/}
+                        {/*  strokeLinejoin="round"*/}
+                        {/*  strokeWidth="2"*/}
+                        {/*  className="w-4 h-4 ml-2"*/}
+                        {/*  viewBox="0 0 24 24"*/}
+                        {/*  width="16"*/}
+                        {/*  height="16"*/}
+                        {/*>*/}
+                        {/*  <path d="M5 12h14M12 5l7 7-7 7" />*/}
+                        {/*</svg>*/}
                       </a>
                     </div>
                   </div>
@@ -70,19 +70,19 @@ const Home: NextPage<Props> = () => {
                 <div className="p-4 lg:w-1/2 md:w-full">
                   <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
                     <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 flex-shrink-0">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-8 h-8"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                      >
-                        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                      </svg>
+                      {/*<svg*/}
+                      {/*  fill="none"*/}
+                      {/*  stroke="currentColor"*/}
+                      {/*  strokeLinecap="round"*/}
+                      {/*  strokeLinejoin="round"*/}
+                      {/*  strokeWidth="2"*/}
+                      {/*  className="w-8 h-8"*/}
+                      {/*  viewBox="0 0 24 24"*/}
+                      {/*  width="24"*/}
+                      {/*  height="24"*/}
+                      {/*>*/}
+                      {/*  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />*/}
+                      {/*</svg>*/}
                     </div>
                     <div className="flex-grow">
                       <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
@@ -94,19 +94,19 @@ const Home: NextPage<Props> = () => {
                       </p>
                       <a className="mt-3 text-blue-500 inline-flex items-center">
                         Learn More
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          className="w-4 h-4 ml-2"
-                          viewBox="0 0 24 24"
-                          width="16"
-                          height="16"
-                        >
-                          <path d="M5 12h14M12 5l7 7-7 7" />
-                        </svg>
+                        {/*<svg*/}
+                        {/*  fill="none"*/}
+                        {/*  stroke="currentColor"*/}
+                        {/*  strokeLinecap="round"*/}
+                        {/*  strokeLinejoin="round"*/}
+                        {/*  strokeWidth="2"*/}
+                        {/*  className="w-4 h-4 ml-2"*/}
+                        {/*  viewBox="0 0 24 24"*/}
+                        {/*  width="16"*/}
+                        {/*  height="16"*/}
+                        {/*>*/}
+                        {/*  <path d="M5 12h14M12 5l7 7-7 7" />*/}
+                        {/*</svg>*/}
                       </a>
                     </div>
                   </div>
