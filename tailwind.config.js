7f84044086fca62681801a547d2b2614169bbc37
reeq20 <rikusugawara@fabrec.jp>
2021-01-04 20:52:49 +0900

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   purge: [
-    "./pages/**/*.tsx",
-    "./components/**/*.tsx",
-    "./pages/**/*.mdx",
-    "./components/**/*.mdx",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
